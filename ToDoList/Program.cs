b5c3710ee2115da52374f529df5bc4721f504b9c
Nikita Kharitonov <nikitaharitonov863@gmail.com>
2024-11-26 17:10:07 +0300

    Добавление класса Task и функционала -Добавление задачи-

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace ToDoApp
+{
+
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Task(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+
+    public class ToDoList
+    {
+        private List<Task> tasks = new List<Task>();
+
+        public void AddTask(string taskName)
+        {
+            int taskId = tasks.Count + 1;
+            tasks.Add(new Task(taskId, taskName));
+            Console.WriteLine($"Задача '{taskName}' добавлена.");
+        }   
+    }
+}
